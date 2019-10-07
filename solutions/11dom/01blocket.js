99c72f290f0ac7b6a9dc9478debf9bd06460daca
Jonas Stattin <jonas@vinnovera.se>
2019-10-07 17:53:54 +0200

    Text changes

--- solutions/11dom/01blocket.js
+++ solutions/11dom/01blocket.js
@@ -1,16 +1,17 @@
-// Hitta elementet med ID "item_87363892"
+// 1. Hitta elementet med ID "item_87363892"
 console.log(document.getElementById('item_87363892'))
 
-// Hitta alla element med klassen "pull-left", loopa över listan, och console.log() varje item
+// 2. Hitta alla element med klassen "pull-left", loopa över listan, och console.log() varje item
 const allPullLeft = document.querySelectorAll('.pull-left')
 for (let i = 0; i < allPullLeft.length; i++) {
   console.log(allPullLeft[i])
 }
 
-// Hur många children har elementet med klassen "media-body"?
+// 3. Hur många children har elementet med klassen "media-body"?
 const mediaBody = document.querySelector('.media-body')
 console.log(mediaBody.children.length)
 
+// 4. Traversering med siblings och children
 // Hitta h1-elementet (h1)
 // Utgå från det elementet och hitta previous sibling (header)
 // Hitta första element child i denna sibling (div)
@@ -34,17 +35,17 @@ for (let i = 0; i < allLinks.length; i++) {
   console.log(allLinks[i].firstChild)
 }
 
-// Vad är det som är till salu?
+// 7. Vad är det som är till salu?
 const headerLink = document.querySelector('h1 a')
 console.log(headerLink.firstChild)
 
-// Vad kostar den?
+// 8. Vad kostar den?
 const price = document.getElementsByClassName('list_price')
 console.log(price[0].firstChild)
 
-// När lades produkten upp?
+// 9. När lades produkten upp?
 const time = document.getElementsByTagName('time')
 console.log(time[0].firstChild)
 
-// Hitta elementet med data-attributet data-js
+// 10. Hitta elementet med attributet data-js
 console.log(document.querySelector('[data-js]'))
