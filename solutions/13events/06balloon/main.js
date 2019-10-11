7b9b28051a81f56185ab9cc17b11c42c4f6f3b7e
Jonas Stattin <jonas@vinnovera.se>
2019-10-11 13:09:29 +0200

    Changed to Key

--- solutions/13events/06balloon/main.js
+++ solutions/13events/06balloon/main.js
@@ -2,7 +2,7 @@ const balloon = document.querySelector('#balloon')
 let fontSize = 16
 
 document.addEventListener('keyup', function(event) {
-  if (event.keyCode === 38) {
+  if (event.key === 'ArrowUp') {
     fontSize = fontSize * 2
     if (fontSize >= 512) {
       balloon.textContent = '💥'
